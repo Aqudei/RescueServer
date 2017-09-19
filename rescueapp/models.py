0b89921389f737439ae4d4650d20948024f4d45a
Archie Espelimbergo Cortez <archie.cortez@outlook.com>
2017-09-19 20:57:22 +0800

    ss

--- rescueapp/models.py
+++ rescueapp/models.py
@@ -4,27 +4,27 @@ from django.db import models
 
 
 class EvacuationCenter(models.Model):
-    centerName = models.CharField(max_length=128)
-    address = models.TextField()
-    limit = models.IntegerField(default=100)
+    CenterName = models.CharField(max_length=128)
+    Address = models.TextField()
+    Limit = models.IntegerField(default=100)
 
 
 class Person(models.Model):
-    firstName = models.CharField(max_length=32)
-    middleName = models.CharField(max_length=32)
-    lastName = models.CharField(max_length=32)
-    birthday = models.DateField(blank=True, null=True)
-    bloodType = models.CharField(max_length=4)
-    address = models.TextField(default='', blank=True)
+    FirstName = models.CharField(max_length=32)
+    MiddleName = models.CharField(max_length=32)
+    LastName = models.CharField(max_length=32)
+    Birthday = models.DateTimeField(blank=True, null=True)
+    BloodType = models.CharField(max_length=4)  
+    Address = models.TextField(default='', blank=True)
 
 
 class Incident(models.Model):
-    incidentName = models.CharField(max_length=32)
-    dateOccured = models.DateTimeField(auto_now_add=True)
+    IncidentName = models.CharField(max_length=32)
+    DateOccured = models.DateTimeField(auto_now_add=True)
 
 
 class CheckIn(models.Model):
-    when = models.DateTimeField(auto_now_add=True)
-    incident = models.ForeignKey(Incident, related_name='check_ins')
-    person = models.ForeignKey(Person, related_name='check_ins')
-    center = models.ForeignKey(EvacuationCenter, related_name='check_ins')
+    When = models.DateTimeField(auto_now_add=True)
+    Incident = models.ForeignKey(Incident, related_name='check_ins')
+    Person = models.ForeignKey(Person, related_name='check_ins')
+    Center = models.ForeignKey(EvacuationCenter, related_name='check_ins')
