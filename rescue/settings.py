295a1cc8b99da17a947098704c369a9055f25dda
Aqudei <yodi_cute07@yahoo.com>
2017-09-21 06:51:51 +0800

    aa

--- rescue/settings.py
+++ rescue/settings.py
@@ -101,6 +101,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
